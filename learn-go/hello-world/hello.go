a296cca1760635620dd2b4cf39df6f73ef8f0908
Sergey Povarnin <sergey.povarnin@gmail.com>
2024-03-28 13:33:17 +0200

    Go fundamentals.1: Hello, World!

---
+++ learn-go/hello-world/hello.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+// Hello returns a personalised greeting, defaulting to Hello, world if an empty name is passed.
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
